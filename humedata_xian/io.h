687f71430d1ddfe1fa7dcd634851b589a8a970dd
christian-leufulab <94867651+christian-leufulab@users.noreply.github.com>
2022-07-19 17:26:44 -0400

    Changed bl from percentage to voltage

--- humedata_xian/io.h
+++ humedata_xian/io.h
@@ -2,8 +2,8 @@
  * PINOUT
  * HTS221 INT = 0
  * LPS25 INT = 1
- * XIAN SWITCH = 2
- * GPS SWITCH = 3
+ * XIAN SWITCH = A5
+ * GPS SWITCH = A6
  * SD CHIP SELECT PIN = 4
  * RDY = 6
  * RDY = 7
@@ -11,6 +11,6 @@
  */
 
 // IO Definitions
-const int xian_switch = 2;
-const int gps_switch = 3;
+const int xian_switch = A5;
+const int gps_switch = A6;
 const int sd_cs_pin = 4;
